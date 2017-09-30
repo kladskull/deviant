f569123cbb9ba1103f9d88a3450018a3716ac8cd
Mike Curry <mike.curry@crowdstrike.com>
2017-09-29 23:10:26 -0400

    some cleanup

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,21 +1,8 @@
 <?php declare(strict_types=1); // strict mode
 
+// TODO: Base on environment
 // error reporting & security
 error_reporting(E_ALL | E_STRICT);
-
-/*
-TODO: The following will be implemented by environment in the future.
-# supress php errors
-php_flag display_startup_errors off
-php_flag display_errors off
-php_flag html_errors off
-php_value docref_root 0
-php_value docref_ext 0
-
-# enable PHP error logging
-php_flag  log_errors on
-php_value error_log  app/logs/[environment]-date-time.log
-*/
 
 // vendor
 include '../vendor/autoload.php';
@@ -26,18 +13,20 @@ include '../config/app.php';
 include '../config/csrf_config.php';
 include '../config/database.php';
 
+// pre-loads
+include '../framework/Kernel.php';
+
 // framework
 include '../framework/Auth.php';
 include '../framework/Base.php';
 include '../framework/Cache.php';
 include '../framework/Controller.php';
-include '../framework/Kernel.php';
 include '../framework/Headers.php';
+include '../framework/Http.php';
 include '../framework/HttpKernel.php';
 include '../framework/Middleware.php';
-include '../framework/View.php';
 include '../framework/Validate.php';
-include '../framework/Http.php';
+include '../framework/View.php';
 
 // load Models
 $iterator = new DirectoryIterator(__DIR__ . '/../app/models/');
