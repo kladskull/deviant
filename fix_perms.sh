40a648334c1aa40330189d26d912adf041071c66
root <root@HS-2017090119>
2017-10-17 16:03:27 +0000

    adding directory creations to fix_perms

--- fix_perms.sh
+++ fix_perms.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
+mkdir ./app/log/
+mkdir ./app/templates/config/
+mkdir ./app/templates/cache/
+mkdir ./app/templates/templates_c/
+chown -R www-data:root ./
 chmod -R a=r,u+w,a+X .
 chmod 755 ./fix_perms.sh
-chmod 755 ./tests/run_tests
 chmod -R 644 ./app/log/
+chmod -R 644 ./app/templates/config/
+chmod -R 644 ./app/templates/cache/
+chmod -R 644 ./app/templates/templates_c/
+chmod -R 755 ./app/log
+chmod -R 755 ./app/templates/config
+chmod -R 755 ./app/templates/cache
+chmod -R 755 ./app/templates/templates_c
+
